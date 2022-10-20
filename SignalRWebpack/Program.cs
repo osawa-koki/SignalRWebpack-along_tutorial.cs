0db622bc1d0973350cebedc4c86cc769776475c9
koko <github@koko-campus.com>
2022-10-20 23:33:48 +0900

    ASP.NETメインプログラム部分作った。

--- SignalRWebpack/Program.cs
+++ SignalRWebpack/Program.cs
@@ -1,5 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+
 var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.MapGet("/", () => "Hello World!");
 
